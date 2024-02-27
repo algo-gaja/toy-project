8fc770c6c45bbc0c44c7c1d4787c2efad7d6ce5a
Yermi <kycasdzxc@naver.com>
2024-02-27 22:26:25 +0900

    Create MyCalendar.java

---
+++ MyCalendar/MyCalendar.java
@@ -0,0 +1,29 @@
+package toy;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MyCalendar {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd E");
+        System.out.println(df.format(cal.getTime()));
+        System.out.println("SU MO TU WE TH FR SA");
+
+        int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        cal.set(Calendar.DATE, 1);
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        
+        for (int i = 1; i < dayOfWeek; i++) {
+            System.out.print("   ");
+        }
+        for (int i = 1; i <= lastDay; i++) {
+            System.out.printf("%02d ", i);
+            if (dayOfWeek % 7 == 0) {
+                System.out.println();
+            }
+            dayOfWeek++;
+        }
+    }
+}
